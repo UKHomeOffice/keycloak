95ff83adea8549221cd1f690ca8ea07501398bca
Pontus <contrib@pontusnetworks.com>
2018-02-22 19:48:40 +0000

    fixed logic to kill process and clean up the keys

--- build-local.sh
+++ build-local.sh
@@ -26,10 +26,11 @@ tar xvfz $TARFILE
 ln -s keycloak-$VERSION current
 sed -i 's/jboss.http.port:8080/jboss.http.port:8081/g'  current/standalone/configuration/standalone.xml
 sed -i 's/jboss.https.port:8443/jboss.https.port:15005/g'  current/standalone/configuration/standalone.xml
+sed -i 's|keystore.*|keystore path="/etc/pki/java/localhost.jks"  keystore-password="pa55word" alias="localhost" key-password="pa55word"/>|g' current/standalone/configuration/standalone.xml
 current/bin/standalone.sh -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=${DIR}/keycloak.export.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING&
 KCPID=$!
-sleep 60
-kill $KCPID 
-wait $KCPID
+sleep 40
+pkill -15 -P $KCPID
+wait 
 sed -i 's/jboss.https.port:15005/jboss.https.port:5005/g'  current/standalone/configuration/standalone.xml
 cd $CURDIR
